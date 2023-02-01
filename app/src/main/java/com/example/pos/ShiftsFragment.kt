f7189c65e5e08f556a14bd284cdd2941f35a760f
LanAnhTranRIKAI <anh.tran@rikai.technology>
2023-02-02 06:17:59 +0900

    Code for showing shifts added

--- app/src/main/java/com/example/pos/ShiftsFragment.kt
+++ app/src/main/java/com/example/pos/ShiftsFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.DatePicker
 import androidx.navigation.fragment.findNavController
@@ -45,29 +46,47 @@ class ShiftsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding?.shiftsFragment = this
         binding?.shiftsViewModel = shiftsViewModel
+
+/*        shiftsViewModel.getAllShifts().observe(viewLifecycleOwner, Observer {
+            val adapter = ShiftsAdapter(requireContext(), it)
+            val recyclerView = binding?.shifts
+            recyclerView?.adapter = adapter
+        })*/
         val spinner = binding?.dateSpinner
         shiftsViewModel.getAllShifts().observe(viewLifecycleOwner, Observer { shifts ->
             val dates = mutableListOf<String>()
             shifts.forEach {
                 dates.add(it.shiftDate)
             }
-            val adapter = ArrayAdapter(requireContext(), R.layout.shifts_spinner_item, dates)
+            val datesList = dates.distinct()
+            val adapter = ArrayAdapter(requireContext(), R.layout.shifts_spinner_item, datesList)
             spinner?.adapter = adapter
+            var selectedDate = ""
+            spinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    selectedDate = formatter.format(calendar)
+                }
 
-        })
-        val selectedDate = spinner?.selectedItem.toString()
-        binding?.shiftsTime?.setOnCheckedChangeListener { _, checkedId ->
-            when (checkedId) {
-                R.id.morning_shift -> {
-                    Log.d(TAG, "morning shift")
-                    showShifts(selectedDate, 0)
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    selectedDate = parent?.getItemAtPosition(position).toString()
+                    Log.d(TAG, "dateSelected $selectedDate")
                 }
-                R.id.afternoon_shift -> {
-                    showShifts(selectedDate, 1)
+            }
+            Log.d(TAG, "dateSelected $selectedDate")
+            binding?.shiftsTime?.setOnCheckedChangeListener { _, checkedId ->
+
+                when (checkedId) {
+                    R.id.morning_shift -> {
+                        showShifts(selectedDate, 0)
+                    }
+                    R.id.afternoon_shift -> {
+                        showShifts(selectedDate, 1)
+                    }
+                    R.id.noon_shift -> {
+                        showShifts(selectedDate, 2)
+                    }
                 }
-                R.id.noon_shift -> {
-                    showShifts(selectedDate, 2)
-                }
+
             }
 
 
@@ -76,7 +95,7 @@ class ShiftsFragment : Fragment() {
                 val adapter = ShiftsAdapter(requireContext(), shifts)
                 recyclerView?.adapter = adapter
             })*/
-        }
+        })
     }
 
 /*    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
@@ -103,6 +122,10 @@ class ShiftsFragment : Fragment() {
         shiftsViewModel.getAllShifts().observe(viewLifecycleOwner, Observer { shifts ->
             val shiftsList = mutableListOf<Shift>()
             shifts.forEach { shift ->
+                Log.d(TAG, "sd ${shift.shiftDate}")
+                Log.d(TAG, "sd ${shift.shiftTime}")
+                Log.d(TAG, "sd ${selectedDate}")
+                Log.d(TAG, "sd ${shiftTime}")
                 if ((shift.shiftDate == selectedDate) && (shift.shiftTime == shiftTime)) {
                     shiftsList.add(shift)
                     Log.d(TAG, "shifts $shiftsList")
